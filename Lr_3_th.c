d3d258d11c9c4c16cbedbd3d8ca08c50a3b50125
Nivoter <36511810+Nivoter@users.noreply.github.com>
2018-03-02 16:22:40 +0400

    Update Lr_3_th.c

--- Lr_3_th.c
+++ Lr_3_th.c
@@ -71,6 +71,7 @@
 #define LCD_E PORTDbits.RD3
 #define LCD_RS PORTDbits.RD2
 #define LCD_Data4 LATD // eieoeaeecaoey ii?oia D4-D7 LCD
+
 void lcd_clk(void) /*aaia?aoey eiioeuna ia aoia EN*/
 {
   LCD_E = 1;
@@ -95,7 +96,6 @@ void lcd_putc(char outbyte) /* ioi?aaeou aaiiua (4-aeoiay iia?aoey) */
   LCD_Data4=(LCD_Data4&0x0f)|((outbyte<<4)&0xf0);
   lcd_clk();
 }
-
 void lcd_puts(unsigned char line,const char *p) // *auaia no?iee ia ye?ai*
 {
 	lcd_origin();         // ia?aoia e ioeaaiio aa?ano LCD
@@ -106,10 +106,10 @@ void lcd_puts(unsigned char line,const char *p) // *auaia no?iee ia ye?ai*
 	 p++;                      // oaaee?eou aa?an ia 1
 	}
 }
-void inttolcd(unsigned char posi, unsigned long value) //auaia ia ye?ai cia?aiee ia?a-iaiiuo
+void inttolcd(unsigned char posi, long value) //auaia ia ye?ai cia?aiee ia?a-iaiiuo
 {
 	char buff[16];
-	utoa(buff,value,10);
+	ltoa(buff,value,10);
 	lcd_puts(posi,buff);
 }
 void lcd_init() // eieoeaeecaoey LCD-aenieay
@@ -140,117 +140,79 @@ __delay_ms(1);
   lcd_command(0x02);	// ia?aeuiay iiceoey
   lcd_command(0x01);	// i?enoeou an? niiaa
 }
-void Adc_init()
- {
- TRISA|=0b11111111; //ia?aaia auaiaia RA0, RA1, RA2, RA3, RA5 a ?a?ei aoiaia
 
- TRISE|=0b00000111; //ia?aaia auaiaia RE0, RE1, RE2 a ?a?ei aoiaia
- ADCON1bits.PCFG=0b0111; // eiioeao?aoey aiaeiai-oeo?iauo ii?oia
- ADCON1bits.VCFG=0b00; // iii?iia iai?y?aiea Vss Vdd
- ADCON2bits.ACQT=0b111;// a?aiy i?aia?aciaaiey 20 Tad
- ADCON2bits.ADCS=0b110;// ?anoioa i?aia?aciaaiey Fosc/64 
+volatile int h=0;
+volatile long k=0;
+volatile unsigned char u=0;
 
- ADCON2bits.ADFM=0;//eaaia niauaiea
- ADCON0bits.ADON=1; // iiaoeu AOI aee??ai
- }
-int read_Adc()
- {
- ADCON0bits.GO_DONE=1; // caione i?aia?aciaaiey
- while(ADCON0bits.GO_DONE==1);
- return (ADRESH<<2)+(ADRESL>>6);//caienu ?acoeuoaoa i?aia?aciaaiey
- } 
+void interrupt HIisr (void) 
+{ 
+    if (INTCONbits.INT0IF) 
+    { 
+        __delay_ms(10);// çàäåðæêà äëÿ èñêëþ÷åíèÿ âëèÿíèÿ «äðåáåçãà» êîíòàêòîâ 
+        __delay_ms(10);         
+        __delay_ms(10); 
+        if (PORTBbits.RB0==0) 
+        { 
+            if(u==0)
+            {
+                T0CONbits.TMR0ON=1;
+                u=1;
+                k=0;
+            }
+            else
+            {
+                T0CONbits.TMR0ON=0;
+                INTCONbits.TMR0IF=0; 
+                inttolcd(0x88,k);
+                u=0;
+            }
+        } 
+        LATBbits.LATB3=!LATBbits.LATB3; 
+        INTCONbits.INT0IF=0; 
+    } 
+} 
 
-volatile unsigned long i=0;
-volatile unsigned long g=0;
-volatile unsigned long k=0;
-volatile unsigned long n=0;
+void interrupt low_priority LIisr (void) 
+{ 
 
-unsigned int time_h = 216;
-unsigned int time_l = 239;
+    k++;
+    TMR0H=0xF8; 
+    TMR0L=0x2F;
+    LATBbits.LATB3=0; 
+    INTCONbits.TMR0IF=0; 
+} 
 
-//unsigned int time_h = 39;
-//unsigned int time_l = 16;
 
-
-void interrupt HIisr (void)
-{
- if (INTCONbits.INT0IF)
- {
- __delay_ms(10);// задержка для исключения влияния «дребезга» контак-
- __delay_ms(10);
- __delay_ms(10);
-    if (PORTBbits.RB0==0)
+void main(void) { 
+    lcd_init();
+    inttolcd(0x80,h);
+    T0CONbits.TMR0ON=0; 
+    INTCONbits.GIEH=1; //ðàçðåøåíèå âûñîêîóðîâíåâûõ ïðåðûâàíèé 
+    INTCONbits.GIEL=1; //ðàçðåøåíèå íèçêîóðîâíåâûõ ïðåðûâàíèé 
+    TRISBbits.RB3=0;//íàñòðîéêà RB3 íà âûõîä 
+    TRISBbits.RB0=1;//íàñòðîéêà RB0 íà âõîä 
+    RCONbits.IPEN=1; // ðàçðåøåíèå äâóõóðîâíåâûõ ïðåðûâàíèé 
+    INTCON2bits.INTEDG0=0; //ïðåðûâàíèå ïî íèñïàäàþùåìó ôðîíòó ñèãíàëà íà âõîäå INT0 
+    INTCONbits.INT0IF=0; //îáíóëåíèå ôëàãà ïðåðûâàíèÿ îò âíåøíåãî èñòî÷íèêà 
+    INTCONbits.GIEH=1; //ðàçðåøåíèå âûñîêîóðîâíåâûõ ïðåðûâàíèé 
+    INTCONbits.INT0IE=1;//ðàçðåøåíèå ïðåðûâàíèÿ îò âíåøíåãî èñòî÷íèêà   
+    TRISBbits.RB3=0;//íàñòðîéêà RB3 íà âûõîä 
+    T0CONbits.T08BIT=0;//íàñòðîéêà òàéìåðà ¹0 íà 16-áèòíûé ðåæèì ðàáîòû 
+    T0CONbits.PSA=1;//ðàçðåøåíèå èñïîëüçîâàòü ïðåääåëèòåëü 
+    T0CONbits.T0PS=0b010;//ïðåääåëèòåëü ðàâåí 8
+    T0CONbits.T0CS=0;//âûáîð âíóòðåííåãî èñòî÷íèêà òàêòîâûõ èìïóëüñîâ 
+    TMR0H=0xF8; 
+    TMR0L=0x2F;
+    RCONbits.IPEN=1; // ðàçðåøåíèå äâóõóðîâíåâûõ ïðåðûâàíèé 
+    INTCON2bits.TMR0IP=0;//ïðèñâîåíèå ïðåðûâàíèþ íèçêîãî ïðèîðèòåòà 
+    INTCONbits.TMR0IF=0;//îáíóëåíèå ôëàãà ïðåðûâàíèÿ ïî ïåðåïîëíåíèþ òàéìåðà 0 
+    INTCONbits.TMR0IE=1;//ðàçðåøåíèå ïðåðûâàíèÿ ïî ïåðåïîëíåíèþ òàéìåðà 0 
+    INTCON2bits.RBIP=0;
+    lcd_puts(0x80,"Measure=");
+    lcd_puts(0xC0,"Current=");
+    while(1)
     {
-        if(T0CONbits.TMR0ON==1)
-        {
-            T0CONbits.TMR0ON=0;
-            
-        }
-        else
-        {
-       T0CONbits.TMR0ON=1;
-       TMR0H=time_h;
-       TMR0L=time_l;
-        }
-       
-        
-    }
-    
-    __delay_ms(10);
-   inttolcd(0x87, k);
-   
-  
- } 
- INTCONbits.INT0IF=0;
-}
-
-void interrupt low_priority LIisr (void)
-{
-    
-        INTCONbits.TMR0IF=0;
-        TMR0H=time_h;
-        TMR0L=time_l;
-        
-        k++;
-    
-}
-int main(int argc, char** argv) {
-lcd_init();
-TRISBbits.RB3=0;//íàñòðîéêà RB3 íà âûõîä
-T0CONbits.T08BIT=0;//íàñòðîéêà òàéìåðà ¹0 íà 16-áèòíûé ðåæèì ðàáîòû
-T0CONbits.PSA=1;//ðàçðåøåíèå èñïîëüçîâàòü ïðåääåëèòåëü
-T0CONbits.T0PS=0b001;//ïðåääåëèòåëü ðàâåí 256
-T0CONbits.T0CS=0;//âûáîð âíóòðåííåãî èñòî÷íèêà òàêòîâûõ èìïóëüñîâ
-T0CONbits.TMR0ON=0;
-//çàïèñü ìëàäøåãî áàéòà íà÷àëüíîãî çíà÷åíèÿ 
-//T0CONbits.TMR0ON=1;
-RCONbits.IPEN=1; // ðàçðåøåíèå äâóõóðîâíåâûõ ïðåðûâàíèé
-INTCON2bits.TMR0IP=0;//ïðèñâîåíèå ïðåðûâàíèþ íèçêîãî ïðèîðèòåòà
-INTCONbits.TMR0IF=0;//îáíóëåíèå ôëàãà ïðåðûâàíèÿ ïî ïåðåïîëíåíèþ òàéìåðà 0
-INTCONbits.GIEH=1; //ðàçðåøåíèå âûñîêîóðîâíåâûõ ïðåðûâàíèé
-INTCONbits.GIEL=1; //ðàçðåøåíèå íèçêîóðîâíåâûõ ïðåðûâàíèé
-INTCONbits.TMR0IE=1;//ðàçðåøåíèå ïðåðûâàíèÿ ïî ïåðåïîëíåíèþ òàéìåðà 0
-TRISBbits.RB3=0;//настройка RB3 на выход
- TRISBbits.RB0=1;//настройка RB0 на вход
- RCONbits.IPEN=1; // разрешение двухуровневых прерываний
- INTCON2bits.INTEDG0=0; //прерывание по ниспадающему фронту сигнала
- INTCONbits.INT0IF=0; //обнуление флага прерывания от внешнего источ-
- INTCONbits.GIEH=1; //разрешение высокоуровневых прерываний
- INTCONbits.INT0IE=1;//разрешение прерывания от внешнего источника
- LATBbits.LATB3=1; 
- while(1)
- {
-     //i=(TMR0H<<8)|TMR0L;
-     //i=(i<<0);
-      
-          inttolcd(0xC7, k);
-          
-      /*if(i>100000000000)
-      {
-          g++;
-          i=0;
-          
-      }
-      inttolcd(0x80, g);*/
- } 
-}
+        inttolcd(0xC8,k);
+    } 
+}    
